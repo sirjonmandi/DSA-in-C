adbcaf4039ce1345fe340a78de33b06b0e981722
Sirjoon Mandi <sirjonmandi13@gimal.com>
2024-02-23 21:54:10 +0530

    insertion function modified and deletion function added

--- Circular_LInkedList_Insersiton_At_Any_Position.c
+++ Circular_LInkedList_Insersiton_At_Any_Position.c
@@ -1,53 +1,107 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
+// Circular Linked List insertion and deletion at any position 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 void add(int);
 void display();
-struct node{
+struct node
+{
     int data;
     struct node *next;
 };
-struct node *head,*tail = NULL;
+struct node *head, *tail = NULL;
 void ins_at_pos()
 {
-    struct node *ptr,*x,*y,*z;
-    int c=1,pos,count = 1;
-    y = (struct node*)malloc(sizeof(struct node));
+    struct node *ptr, *x, *y, *z;
+    int c = 1, pos, count = 1;
+    y = (struct node *)malloc(sizeof(struct node));
     if (head == NULL)
     {
         printf("cannot enter an element at this place");
     }
     printf("enter a data for a particular possition ");
-    scanf("%d",&y->data);
+    scanf("%d", &y->data);
     printf("\nenter the possition to be inserted :");
-    scanf("%d",&pos);
-    x=head;
+    scanf("%d", &pos);
+    x = head;
     ptr = head;
-    while(ptr->next != head)
+    if (pos == 1)
     {
+        while (ptr->next != head)
+        {
+            // count++;
+            ptr = ptr->next;
+        }
+        ptr->next = y;
+        y->next = head;
+        head = y;
+    }
+    else
+    {
+
+        while (ptr->next != head)
+        {
+            count++;
+            ptr = ptr->next;
+        }
         count++;
-        ptr = ptr->next;
+        if (pos > count)
+        {
+            printf("out of bound\n");
+            return;
+        }
+        while (c < pos)
+        {
+            z = x;
+            x = x->next;
+            c++;
+        }
+        y->next = x;
+        z->next = y;
     }
-    count++;
-    if (pos>count)
+}
+void del_at_pos()
+{
+    struct node *x, *y;
+    int c = 1, pos;
+    printf("\n Enter the position to be deleted:");
+    scanf("%d", &pos);
+    x = head;
+    if (head == NULL)
+        printf("\n List is empty");
+    else if (pos == 1)
     {
-        printf("out of bound\n");
-        return ;
+        while (x->next != head)
+        {
+            x = x->next;
+        }
+        y = head;
+        x->next = y->next;
+        head = y->next;
+        free(y);
     }
-    while(c<pos)
+    else
     {
-        z=x;
-        x=x->next;
-        c++;
+
+        x = head;
+
+        while (c < pos)
+        {
+            y = x;
+            x = x->next;
+            c++;
+        }
+        y->next = x->next;
+        free(x);
     }
-    y->next = x;
-    z->next = y;
 }
 int main()
 {
     add(1);
     add(2);
     add(3);
+    add(4);
+    add(5);
     display();
     ins_at_pos();
     display();
@@ -55,18 +109,19 @@ int main()
 }
 void add(int value)
 {
-    struct node *newNode = (struct node*)malloc(sizeof(struct node));
-    newNode->data = value; 
+    struct node *newNode = (struct node *)malloc(sizeof(struct node));
+    newNode->data = value;
     if (head == NULL)
     {
         head = newNode;
         tail = newNode;
-    }else{
+    }
+    else
+    {
         tail->next = newNode;
         tail = newNode;
         tail->next = head;
     }
-    
 }
 void display()
 {
@@ -74,15 +129,14 @@ void display()
     if (head == NULL)
     {
         printf("list is empty!");
-    }else{
+    }
+    else
+    {
         printf("node of the circular linkedlist is :\n");
         do
         {
-            printf("%d \n",current->data);
-            current =current->next;
-
-        } while (current->next != head);
-        
+            printf("%d \n", current->data);
+            current = current->next;
+        } while (current != head);
     }
-    
 }
