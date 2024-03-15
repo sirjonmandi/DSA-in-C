52fd5a049c5041d9f48335fcceafbc3804af54e8
Sirjoon Mandi <sirjonmandi13@gimal.com>
2024-03-15 15:32:34 +0530

    circular doublylinkedlist updated

--- circular_double_linkedlist.c
+++ circular_double_linkedlist.c
@@ -34,69 +34,100 @@ void addNode()
             temp1->next = temp;
             temp->prev = temp1;
         }
-    }else
-        {
-            printf("\nMemory not avilable...node allocation is not possible");
-        }
+    }
+    else
+    {
+        printf("\nMemory not avilable...node allocation is not possible");
+    }
 }
 void display(node *temp)
 {
-    // now temp1 is head basically
-    node *temp1 = temp;
-    printf("The list is as follows :\n%d ", temp->data);
-    temp = temp->next;
-    // which not circle back to head node
-    while (temp != temp1)
+    if (head != NULL)
     {
-        printf("%d ", temp->data);
+        // now temp1 is head basically
+        node *temp1 = temp;
+        printf("The list is as follows :\n%d ", temp->data);
         temp = temp->next;
+        // which not circle back to head node
+        while (temp != temp1)
+        {
+            printf("%d ", temp->data);
+            temp = temp->next;
+        }
     }
-    return;
+    else{
+        printf("\nUnderflow\n");
+    }
 }
 
 void inst_beg()
 {
+    if (head == NULL)
+    {
+        printf("\nlist is not created you cann't insert at first \n");
+        return;
+    }
+    
     node *ptr = (node *)malloc(sizeof(node));
     node *p = head->prev;
     printf("\n enter value to insert at first : ");
-    scanf("%d",&ptr->data);
+    scanf("%d", &ptr->data);
     ptr->prev = head->prev;
     ptr->next = head;
-    while(p->next != head)
+    while (p->next != head)
     {
-        p=p->next;
+        p = p->next;
     }
     head = ptr;
     p->next = ptr;
 }
 void inst_end()
 {
+    if (head == NULL)
+    {
+        printf("\nlist is not created you cann't insert at end :) \n");
+        return;
+    }
+    
     node *ptr = (node *)malloc(sizeof(node));
     node *p = head->prev;
     printf("\n enter value to insert at end position  : ");
-    scanf("%d",&ptr->data);
-    while(p->next != head)
+    scanf("%d", &ptr->data);
+    while (p->next != head)
     {
-        p=p->next;
+        p = p->next;
     }
     p->next = ptr;
-    ptr->prev =p;
+    ptr->prev = p;
     ptr->next = head;
-
 }
 int main()
 {
-    int choice;
-    // building circular double linked list as input
-    do
-    {
-        addNode();
-        printf("\nIf you wish to add more data on the list enter 1 : ");
+    int choice = 1;
+    while (choice != 5) {
+        printf("\ncircular linked list program \n");
+        printf("1.add node\n");
+        printf("2.add node at first position\n");
+        printf("3.add node at end position\n");
+        printf("4.display\n");
+        printf("5.exit\n");
+        printf("\nenter your choice : ");
         scanf("%d", &choice);
-    } while (choice == 1);
-
-    display(head);
-    inst_end();
-    display(head);
+        switch (choice)
+        {
+        case 1:addNode();
+            break;
+        case 2:inst_beg();
+            break;
+        case 3:inst_end();
+            break;
+        case 4:display(head);
+            break;
+        case 5:exit(1);
+            
+        default:printf("wrong choice");
+            break;
+        }
+    }
     return 0;
 }
---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,1 @@
+    // which not circle back to head node
